12810603377310bfef186390d31700761e54d30d
liaozhenhua <liaozhenhua@qiyi.com>
2021-02-19 22:46:58 +0800

    最长不重复子字符串

--- src/main/java/com/lzh/string/L3LongSubStringNoRepeat.java
+++ src/main/java/com/lzh/string/L3LongSubStringNoRepeat.java
@@ -11,7 +11,7 @@ import java.util.Set;
  * ******************************************************/
 public class L3LongSubStringNoRepeat {
     //利用滑动窗口的概念
-    public static void main(String[] args) {
+    public static void emthod(String[] args) {
         String a = "pwwkew";//"a";//"abcabcbb"; //dvdf//"pwwkew" //tm
         int i = 0, j = 0;
         int maxLength = 0;
@@ -32,19 +32,29 @@ public class L3LongSubStringNoRepeat {
 
     }
 
-    public int method2(String s) {
-        Map<Character, Integer> characters = new HashMap<>();
 
-        int i = 0, len = 0, j = 0;
-        while (j < s.length()) {
-            char c = s.charAt(j);
-            if (characters.containsKey(c)) {
-                i = Math.max(characters.get(c) + 1, i);
+    public static int method3(String s) {
+        int winStart = 0, len = 0;
+        Map<Character, Integer> window = new HashMap<>();
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+            //有重复数据，需要重置滑动窗口开始位置
+            if (window.containsKey(c)) {
+                //避免 abba 这种情况，将窗口往前延伸
+                winStart = Math.max(window.get(c) + 1,winStart);
             }
-            len = Math.max(j - i + 1, len);
-            characters.put(c, j);
-            j++;
+            len = Math.max(end - winStart + 1, len);
+            window.put(c, end);
         }
         return len;
     }
+
+    public static void main(String[] args){
+//        System.out.println(method3("abcabc"));
+//        System.out.println(method3("aaaa"));
+//        System.out.println(method3("pwwkew"));
+        System.out.println(method3("abba"));
+    }
+
+
 }
