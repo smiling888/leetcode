e4dddb1c16589b9ff0f6517a5940c95ce719f1d2
liaozhenhua <liaozhenhua@qiyi.com>
2021-02-20 21:11:10 +0800

    颜色排序

--- src/main/java/com/lzh/array/L75sortColors.java
+++ src/main/java/com/lzh/array/L75sortColors.java
@@ -22,4 +22,37 @@ public class L75sortColors {
             color++;
         }
     }
+
+    /**
+     * 遍历一次。只有遇到0就往左边放，遇到2就往右边放
+     * 定义3个指针
+     * 10210 当i=2时 01210 此时交换后，01012 此时还需要i需要往回重新指
+     *
+     * @param nums
+     */
+    public void sortColors2(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        for (int i = 0; i < right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, left, i);
+                left++;
+            }
+            if (nums[i] == 2) {
+                swap(nums, right, i);
+                right--;
+
+                if (nums[i] != 1) {
+                    i--;
+                }
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
