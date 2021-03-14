68c2cb7235e15829908e91733109ad52641eb309
liaozhenhua <liaozhenhua@qiyi.com>
2021-03-14 22:07:49 +0800

    官方解答

---
+++ src/main/java/com/lzh/array/L46permute.java
@@ -0,0 +1,59 @@
+package com.lzh.array;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/******************************************************
+ * @Date: 2021-03-13
+ * @Author: lzh <lzhhua110@126.com>
+ * ******************************************************/
+public class L46permute {
+
+    public List<List<Integer>> permute(int[] nums) {
+        int len = nums.length;
+        // 使用一个动态数组保存所有可能的全排列
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>(len);
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+    private void dfs(int[] nums, int len, int depth,
+                     Deque<Integer> path, boolean[] used,
+                     List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!used[i]) {
+                path.addLast(nums[i]);
+                used[i] = true;
+
+                System.out.println("  递归之前 => " + path);
+                dfs(nums, len, depth + 1, path, used, res);
+
+                used[i] = false;
+                path.removeLast();
+                System.out.println("递归之后 => " + path);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        L46permute solution = new L46permute();
+        List<List<Integer>> lists = solution.permute(nums);
+        System.out.println(lists);
+    }
+
+}
