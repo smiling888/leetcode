a7f541d6731d601588ec24cedfc5fc3b3584ebdc
liaozhenhua <liaozhenhua@kuaishou.com>
2023-09-06 20:08:47 +0800

    L290

--- src/main/java/com/lzh/string/LRC092MinFlipsMonoIncr.java
+++ src/main/java/com/lzh/string/LRC092MinFlipsMonoIncr.java
@@ -1,7 +1,7 @@
 package com.lzh.string;
 
 /**
- * Created on 2021-081-13
+ * Created on 2021-081git -13
  * https://leetcode-cn.com/problems/check-if-string-is-a-prefix-of-array/
  */
 public class LRC092MinFlipsMonoIncr {
